a4af81af704c792f5d3954fede12858521279e69
mofeng-git <3285338098@qq.com>
2022-02-01 06:18:15 +0800

    更新

--- myDjango/first/views.py
+++ myDjango/first/views.py
@@ -4,16 +4,17 @@ from django.shortcuts import render
 
 def sayHello(request):
   current_time = datetime.datetime.now()
-  context          = {}
-  context['centext'] = 'Hello World!'
-  return render(request, 'index.html', context)
+  context1 = {}
+  context1['centext'] = 'Hello World!'
+  return render(request, 'index.html', context1)
        
 def signUp(request): 
   return HttpResponse("还未开发完成。")
 
 def signIn(request): 
-  return HttpResponse("还未开发完成。")
-  pass
+  centext2 = {}
+  centext2['apptitle'] = '简单云储存'
+  return render(request, 'signIn.html', centext2)
 
 def loginOut(request): 
   return HttpResponse("还未开发完成。")
