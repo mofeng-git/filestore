4123bac6f665bd6514349449cf81d91b4b8c574c
mofeng-git <3285338098@qq.com>
2022-01-31 16:04:24 +0800

    修正错误

--- myDjango/first/views.py
+++ myDjango/first/views.py
@@ -4,14 +4,14 @@ from django.http import HttpResponse
 def sayHello(request):
   current_time = datetime.datetime.now()
   context          = {}
-  context['centext'] = 'Hello World!' + current_time
-    return render(request, 'index.html', context)
-  return HttpResponse(html)
+  context['centext'] = 'Hello World!'
+  return render(request, 'index.html', context)
        
 def signUp(request): 
   return HttpResponse("还未开发完成。")
 
 def signIn(request): 
+  return HttpResponse("还未开发完成。")
   pass
 
 def loginOut(request): 
