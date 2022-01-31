f260ecf64923737c9a98f6af483088bab1a7dc64
mofeng-git <3285338098@qq.com>
2022-01-31 23:45:31 +0800

    更新

--- myDjango/first/views.py
+++ myDjango/first/views.py
@@ -1,6 +1,7 @@
 import datetime
 from django.http import HttpResponse
- 
+from django.shortcuts import render
+
 def sayHello(request):
   current_time = datetime.datetime.now()
   context          = {}
